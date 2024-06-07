a3a0ff2dc5e2d6718f624b95766fe418a8ee2c39
Austin Denofrio DT <denofrioaustin@gmail.com>
2024-06-07 12:48:35 -0400

    Added sidebar and main content section

---
+++ Tasket/wwwroot/Tasket.lib.module.js
@@ -0,0 +1,22 @@
+﻿export function afterWebStarted(blazor) {
+    showActiveAccordion();
+    window.dispatchEvent(new Event('resize'));
+
+    blazor.addEventListener('enhancedload', () => {
+        KTComponents.init();
+        KTAppLayoutBuilder.init();
+        KTThemeModeUser.init();
+        KTThemeMode.init();
+        KTAppSidebar.init();
+
+        showActiveAccordion();
+        window.dispatchEvent(new Event('resize'));
+    });
+}
+
+function showActiveAccordion() {
+    const activeAccordion = document.querySelector('.menu-accordion:has(.active)');
+    if (activeAccordion) {
+        activeAccordion.classList.add('show', 'here');
+    }
+}
